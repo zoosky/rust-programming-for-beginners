5657bdc5ca82653ffcdd52b6b20f7246dfd8933d
Zoo Sky <zoosky@gmail.com>
2023-09-17 14:58:52 +0200

    fist solution

--- src/bin/a12.rs
+++ src/bin/a12.rs
@@ -4,10 +4,61 @@
 // * Print the characteristics of a shipping box
 // * Must include dimensions, weight, and color
 //
-// Notes:
-// * Use a struct to encapsulate the box characteristics
 // * Use an enum for the box color
-// * Implement functionality on the box struct to create a new box
-// * Implement functionality on the box struct to print the characteristics
+#[derive(Debug)]
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
 
-fn main() {}
+struct Dimensions {
+    length: f32,
+    width: f32,
+    height: f32,
+}
+
+// * Use a struct to encapsulate the box characteristics
+struct ShoppingBox {
+    dimensions: Dimensions,
+    color: Color,
+    weight: f32,
+}
+
+impl ShoppingBox {
+    fn new(dimensions: Dimensions, color: Color, weight: f32) -> Self {
+        Self {
+            dimensions,
+            color,
+            weight,
+        }
+    }
+
+    fn print_specification(&self) {
+        println!(
+            "Dimensions: {} x {} x {}",
+            self.dimensions.length, self.dimensions.width, self.dimensions.height
+        );
+        println!("Color: {:?}", self.color);
+        println!("Weight: {}", self.weight);
+    }
+}
+// Notes:
+// * Implement functionality on the box struct to create a new box
+
+fn main() {
+    // * Implement functionality on the box struct to print the characteristics
+    let dim = Dimensions {
+        length: 4.0,
+        width: 5.0,
+        height: 6.0,
+    };
+    let sbox1 = ShoppingBox {
+        dimensions: dim,
+        color: Color::Red,
+        weight: 34.5,
+    };
+    let sbox2 = ShoppingBox::new(dim, Color::Green, 12.0);
+    sbox1.print_specification();
+    sbox2.print_specification();
+}
